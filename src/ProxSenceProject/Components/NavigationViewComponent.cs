38e9caf3c1a87107414d0fd67636f51550f95f75
Скоробогатов Святослав <45883075+CarbonSkorobogatov@users.noreply.github.com>
2019-05-25 12:06:38 +0300

    Add files via upload

---
+++ src/ProxSenceProject/Components/NavigationViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using ProxSenceProject.Models.Interfaces;
+
+namespace ProxSenceProject.Components
+{
+    public class NavigationViewComponent : ViewComponent
+    {
+            private IProjectData _projectData;
+
+            public NavigationViewComponent(IProjectData projectData)
+            {
+                _projectData = projectData;
+            }
+
+            public IViewComponentResult Invoke()
+            {
+                ViewBag.SelectedCategory = RouteData?.Values["category"];
+                return View(_projectData.ProjectData
+                    .Select(x => x.ProjectCategory)
+                    .Distinct()
+                    .OrderBy(x => x));
+            }
+    }
+}
