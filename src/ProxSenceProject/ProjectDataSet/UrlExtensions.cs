9cb2cda01ee517ff65a7997b6861aa8943fd3858
Скоробогатов Святослав <45883075+CarbonSkorobogatov@users.noreply.github.com>
2019-05-25 12:10:07 +0300

    Add files via upload

---
+++ src/ProxSenceProject/ProjectDataSet/UrlExtensions.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Сесссиииииия!
+ */
+
+using Microsoft.AspNetCore.Http;
+
+namespace ProxSenceProject.ProjectDataSet
+{
+    public static class UrlExtensions
+    {
+        public static string PathAndQuery(this HttpRequest request) =>
+           request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
+
+    }
+}
